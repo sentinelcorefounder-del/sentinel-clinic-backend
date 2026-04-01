31ef7cf0231e08a4d4b4ef9aa58601805244f006
sentinelcorefounder-del <sentinel.core.founder@gmail.com>
2026-04-01 05:13:51 +0100

    Register organization models in admin

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Organization
 
-# Register your models here.
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("clinic_id", "name", "contact_email", "is_active", "created_at")
+    search_fields = ("clinic_id", "name", "contact_email")
+    list_filter = ("is_active",)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserOrganization
 
-# Register your models here.
+
+@admin.register(UserOrganization)
+class UserOrganizationAdmin(admin.ModelAdmin):
+    list_display = ("user", "organization")
+    search_fields = ("user__username", "organization__clinic_id", "organization__name")
